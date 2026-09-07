2ba03104b232f8eabe0920234255020197f0e78a
Sanyamkothari820 <sanyamkothari820@gmail.com>
2026-09-07 16:19:17 +0545

    Linking the dashboard's html file in views.py

--- businesses/views.py
+++ businesses/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Business
+from backend.integrating_financial_analysis.final_analysis import analyze_business
+
+def dashboard(request, business_id):
+
+    business = get_object_or_404 (Business,id=business_id)
+
+    analysis = analyze_business (business)
+
+    context = {"business": business, "analysis": analysis}
+
+    return render(request, "dashboard.html", context)
