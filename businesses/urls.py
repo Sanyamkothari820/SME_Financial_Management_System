b1cd114a52be38cf7e57de1964a9f106b4fafa04
Sanyamkothari820 <sanyamkothari820@gmail.com>
2026-09-09 15:42:56 +0545

    creating the url linking to the particular business

--- businesses/urls.py
+++ businesses/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from . import views
-
+# It creates the url which links to the path of the dashboard along with the business id of the business 
 urlpatterns = [
     path(
         "dashboard/<int:business_id>/",
