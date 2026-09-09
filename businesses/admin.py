bf3a3eafa6c658cb81172eadfdd0f22117e97297
Sanyamkothari820 <sanyamkothari820@gmail.com>
2026-09-09 15:40:57 +0545

    Set up django admin

--- businesses/admin.py
+++ businesses/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Business, Transaction, FinancialPosition
 
+#Setting up django admin by registering the classes mentioned in the models file, which helps in managing the database records through its admin panel
 admin.site.register(Business)
 admin.site.register(Transaction)
 admin.site.register(FinancialPosition)
