f881b52afb113efa6696ed15be4ab2a4b9cdf8bd
Sanyamkothari820 <sanyamkothari820@gmail.com>
2026-09-09 15:43:45 +0545

    Add the values to show in the dashboard

--- businesses/views.py
+++ businesses/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Business
+from .models import Business, Transaction
 from backend.integrating_financial_analysis.final_analysis import analyze_business
+from backend.analytics.calculations import calc_monthly_financials
 
 
 def dashboard(request, business_id):
+
+    '''
+        This function takes business_id along with the request as input arguments, and by using that id, it tells the django to find the business with the id and then calls the analyze_business function with that particular business name and returns the request along with its content in the dashboard of the websites
+        
+    '''
 
     business = get_object_or_404(
         Business,
@@ -12,6 +18,22 @@ def dashboard(request, business_id):
     )
 
     analysis = analyze_business(business)
+
+    transactions = Transaction.objects.filter(
+        business=business
+    )
+
+    monthly_financials = calc_monthly_financials(transactions)
+
+    monthly_data = []
+
+    for month, row in monthly_financials.iterrows():
+
+        monthly_data.append({
+            "month": str(month),
+            "revenue": float(row["revenue"]),
+            "expenses": float(row["expenses"]),
+        })
 
     context = {
         "business": business,
@@ -31,6 +53,8 @@ def dashboard(request, business_id):
         "health_status": analysis["health_status"],
 
         "warnings": analysis["warnings"],
+
+        "monthly_data": monthly_data,
     }
 
     return render(
@@ -38,3 +62,4 @@ def dashboard(request, business_id):
         "dashboard.html",
         context
     )
+
